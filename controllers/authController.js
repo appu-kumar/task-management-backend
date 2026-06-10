37404dd9a13508d3bc9a5270bbbaa442765eb80f
Appu Kumar <appu6688kumar@gmail.com>
2026-06-10 19:39:31 +0530

    add activity and admin routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -135,3 +135,15 @@ export const logout = async (req, res) => {
     message: "Logged out successfully",
   });
 };
+
+export const getMe = async (req, res) => {
+  try {
+    res.json({
+      user: req.user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,14 @@
-import {
-  register,
-  login,
-  logout,
-} from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/authController.js";
 import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
+import { getMe } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.get("/me", authMiddleware, getMe);
 
 export default router;
